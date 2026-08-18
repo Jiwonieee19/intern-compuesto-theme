bfcc9c192e49aaaa5f755fdaeba2b125b7ec5ac2
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-08-18 23:02:32 +0800

    update: home page finalization

--- sql/local-20260816-164945.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fc57b893bc6fd51d9ac505567aa02a66e50a32a7eb8d7da549f8d196c3f47338
-size 16255742
---
+++ sql/local-20260818-145820.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9727cacaf894b1125f1e2cd1bd8ff2240a4826a278beff998499c36fdaa35c29
+size 120440516
