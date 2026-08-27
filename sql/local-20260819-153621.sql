c73d872c9b7005e9a1bb489d227033b433c86837
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-08-27 23:08:16 +0800

    chore: about me finalize

--- sql/local-20260819-153621.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:30527b7b62881e1b5c766ea15a438263256695ba1d387e2716f1e953e1548d77
-size 83368704
---
+++ sql/local-20260827-150616.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e971d4358141ba4b8756ff8269584c0633968b4b42046abc4988805a6d15eef1
+size 141080912
