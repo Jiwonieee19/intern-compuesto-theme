2b536f64564351f1da9fe4704e27bd91be5f966a
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-09-07 14:18:01 +0800

    feat: new design for skill section

--- sql/local-20260810-155947.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:341d8969ef536d51de45b6aa190dd4b26a21074161f7204eab84687454d6615f
-size 153982661
---
+++ sql/local-20260907-061147.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd8168ee628d0c48ff057a5c1083d7bb99dd1ba09cdbc31f1e1b964728240f56
+size 227405189
