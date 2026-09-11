8ec29ca6d5edd0eb8da21f93f614a7b5d0b993c0
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-09-11 13:43:52 +0800

    chore: responsive about me

--- sql/local-20260907-203425.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fbe76326fc7468e634623732b09897879043fc7356de5d1eec4290e931882d64
-size 319798455
---
+++ sql/local-20260911-054251.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dfb205fe102833fdd06a0235d2fc36414edbce99fe87e656e29ee41d62bee47e
+size 349992824
