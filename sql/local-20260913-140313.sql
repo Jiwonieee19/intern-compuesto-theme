0c1a6fd1a64e1324d07ccd1833b86424f1b3131d
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-09-15 23:07:02 +0800

    chore: links for all existing buttons

--- sql/local-20260913-140313.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a1fc716a1f850d24ef79d98bfc4619ad7859a0ca504e9cbc943d42f9368184fc
-size 369313293
---
+++ sql/local-20260915-150503.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf09ed4370eaa8cde72c2c3480509691b975ebb86bdcb665fa6f1f7016e453be
+size 372129366
