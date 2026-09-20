ad0620f0e06f3d24058ed21d3848b7bdfca4a7c9
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-09-20 18:09:55 +0800

    chore: responsive project page

--- sql/local-20260920-092235.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7616f0332f47efe32f9625658774b412a9b0d793c560cd5a729a4ba8f4799214
-size 497344110
---
+++ sql/local-20260920-100708.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db341338fc89398eb817a876d73f6609764a262b3c335ab920d3d2e8526c6588
+size 498159377
